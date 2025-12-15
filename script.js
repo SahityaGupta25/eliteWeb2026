18701fb72886b68af9253aa446d7570267d5f8af
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2025-12-15 12:16:46 +0530

    Added Hamburger functionality and responsiveness as well.

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 // Hamburger menu, animations, form handling will be added later
 
 console.log("Elite Aavas website loaded successfully");
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
