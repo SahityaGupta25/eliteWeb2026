a158efc4f5186994218df5c99eef0c75fa66ca82
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2025-12-16 08:32:46 +0530

    Added smooth scroll

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ console.log("Elite Aavas website loaded successfully");
 
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("navLinks");
+const navItems = navLinks.querySelectorAll("a");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+navItems.forEach(item => {
+  item.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
+
